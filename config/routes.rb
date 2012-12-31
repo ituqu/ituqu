1f8522e9c41abf1e422eb17df5eb439854cacac8
ituqu <wqy874654098@126.com>
2012-12-31 16:07:14 +0800

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Team::Application.routes.draw do
-<<<<<<< HEAD
-  devise_for :users 
-=======
-  #devise_for :users , :controllers => { :registrations => "registrations" }
   devise_for :users
->>>>>>> e43b70edde82838cfff69c982eeca78e6c6713e4
   resources :users, :except => [:index,:new,:create]
   resources :groups
   resources :homes
-  root :to => 'teams#index'
+  root :to => 'groups#index'
 end
